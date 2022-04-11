85a10d331c1bec3759beda30e110f0b17200a2dc
tinegaCollins <collinstinega003@gmail.com>
2022-04-11 05:28:34 +0300

    first.update

--- first/src/App.vue
+++ first/src/App.vue
@@ -1,59 +1,30 @@
 <template>
-    <div id="topbar">
-      <div class="icon"><p>Windows <span>Report</span></p></div>
-      <div class="otherIcon">
-        <div class="search"><img src=".\assets\magnifying-glass-solid.svg" alt="">search</div>
-        <div class="youtube"><img src=".\assets\youtube-brands.svg" alt=""></div>
-      </div>
-      <Navigation/>
-    </div>
+    <topbar/>
+    <Navigation />
 </template>
 
 <script>
 
+import topbar from './components/topbar.vue'
 import Navigation from './components/Navigation.vue'
+import './assets/global.css';
 
 export default {
-  componets: { Navigation }
+  components: { topbar, Navigation},
+  data() {
+    return {
+      message: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    onClick() {
+      alert('You clicked me!')
+    }
+  }
 
 }
 </script>
 
 <style>
-  #topbar{
-    height: 60px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid black;
-  } 
-  #topbar .icon{
-    margin:0 auto 0 30px;
-  }
-  #topbar .icon p{
-    font-size: 2rem;
-  }
-  #topbar .icon p span{
-    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-  }
-  #topbar img{
-    height: 20px;
-    width: auto;
-  }
-  #topbar .otherIcon{
-    display: flex;
-    align-items : center;
-    font-size: 1.4rem;
-  }
-  #topbar .otherIcon .search{
-    margin-right: 20px;
-    display: flex;
-    align-items: center;
-    column-gap: 10px;
-    padding-right: 10px;
-    border-right: 1px solid #333;
-  }
-  #topbar .otherIcon .youtube{
-    margin-right: 20px;
-  }
+  
 </style>
---
+++ first/src/components/topbar.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="topbar">
+      <div class="icon"><p>Windows <span>Report</span></p></div>
+      <div class="otherIcon">
+        <div class="search"><img src="..\assets\magnifying-glass-solid.svg" alt="">search</div>
+        <div class="youtube"><img src="..\assets\youtube-brands.svg" alt=""></div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    #topbar{
+    height: 60px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin-top: 20px;
+  } 
+  #topbar .icon{
+    margin:0 auto 0 30px;
+  }
+  #topbar .icon p{
+    font-size: 2rem;
+    font: "Open Sans", sans-serif;
+    color: #27AEDE;
+  }
+  #topbar .icon p span{
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    color: black;
+  }
+  #topbar img{
+    height: 20px;
+    width: auto;
+  }
+  #topbar .otherIcon{
+    display: flex;
+    align-items : center;
+    font-size: 1.4rem;
+  }
+  #topbar .otherIcon .search{
+    margin-right: 20px;
+    display: flex;
+    align-items: center;
+    column-gap: 10px;
+    padding-right: 10px;
+    border-right: 1px solid #333;
+  }
+  #topbar .otherIcon .youtube{
+    margin-right: 20px;
+  }
+</style>
