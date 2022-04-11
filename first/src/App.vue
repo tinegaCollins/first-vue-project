50bb4b6fb393aa51131f15c617fc6318c1a4292f
tinegaCollins <collinstinega003@gmail.com>
2022-04-11 15:09:22 +0300

    new commit

--- first/src/App.vue
+++ first/src/App.vue
@@ -1,6 +1,9 @@
 <template>
     <topbar/>
     <Navigation />
+    <main>
+        
+    </main>
 </template>
 
 <script>
@@ -17,14 +20,43 @@ export default {
     }
   },
   methods: {
-    onClick() {
-      alert('You clicked me!')
-    }
+    //function to get a random number between two numbers
+    getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    //function to get a random color
+    getRandomColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
+    //create facebook like button
+    createFacebookLikeButton() {
+      var button = document.createElement('div');
+      button.className = 'fb-like';
+      button.setAttribute('data-href', 'https://www.facebook.com/vuejs');
+      button.setAttribute('data-layout', 'button_count');
+      button.setAttribute('data-action', 'like');
+      button.setAttribute('data-size', 'small');
+      button.setAttribute('data-show-faces', 'true');
+      button.setAttribute('data-share', 'true');
+      return button;
+    },
+
   }
 
 }
 </script>
 
 <style>
-  
+  /*center that div*/
+  .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+  }
 </style>
