966d4f26214eecc0bd857c2b161ce3e5b7cc627b
tinegaCollins <collinstinega003@gmail.com>
2022-04-17 17:25:24 +0300

    new commit on routes

--- first/src/App.vue
+++ first/src/App.vue
@@ -2,6 +2,7 @@
     <topbar/>
     <Navigation />
     <main>
+          <sidebar v-if="showSideBar"/>
           <router-view/>
     </main>
 </template>
@@ -10,53 +11,21 @@
 
 import topbar from './components/topbar.vue'
 import Navigation from './components/Navigation.vue'
+import sidebar from './components/sidebar.vue'
 import './assets/global.css';
 
-export default {
-  components: { topbar, Navigation},
-  data() {
-    return {
-      message: 'Welcome to Your Vue.js App'
-    }
-  },
-  methods: {
-    //function to get a random number between two numbers
-    getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-    //function to get a random color
-    getRandomColor() {
-      var letters = '0123456789ABCDEF';
-      var color = '#';
-      for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    },
-    //create facebook like button
-    createFacebookLikeButton() {
-      var button = document.createElement('div');
-      button.className = 'fb-like';
-      button.setAttribute('data-href', 'https://www.facebook.com/vuejs');
-      button.setAttribute('data-layout', 'button_count');
-      button.setAttribute('data-action', 'like');
-      button.setAttribute('data-size', 'small');
-      button.setAttribute('data-show-faces', 'true');
-      button.setAttribute('data-share', 'true');
-      return button;
-    },
-
-  }
-
+export default { 
+  components: { topbar, Navigation, sidebar },
+  props: ['showSideBar'],
 }
 </script>
 
 <style>
-  /*center that div*/
-  .center {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;
+  main{
+    margin-top: 30px;
+    background-color: red;
+    min-height: 80vh;
+    display: flex;
   }
+
 </style>
---
+++ first/src/components/sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="sidebar">
+        <a href="#">random shit</a>
+        <a href="#">random shit</a>
+        <a href="#">random shit</a>
+        <a href="#">random shit</a>
+        <a href="#">random shit</a>
+        <a href="#">random shit</a>
+        <a href="#">random shit</a>
+        <a href="#">random shit</a>
+        <a href="#">random shit</a>
+        <a href="#">random shit</a>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .sidebar{
+    display: flex;
+    flex-direction: column;
+    min-width: 25%;
+    background: greenyellow;
+    justify-content: space-around;
+    align-items: center;
+  }
+  a{
+      text-align: center;
+      width: 100%;
+      height: 100%;
+      padding-top: 10px;
+      color: black;
+      text-decoration: none;
+      transition: all .3s ease-in-out;
+  }
+  a:hover{
+      background: cyan;
+  }
+</style>
