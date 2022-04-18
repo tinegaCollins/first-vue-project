01f4ad57c4784f72c082f6032c13d32454b92c68
tinegaCollins <collinstinega003@gmail.com>
2022-04-18 19:49:41 +0300

    final commit

--- src/views/news.vue
+++ src/views/news.vue
@@ -1,6 +1,25 @@
 <template>
-    <div class="viewsDiv">
-    
+  <div class="viewsDiv">
+     <div class="img">
+       <img src="https://media.istockphoto.com/photos/positivity-puts-you-in-a-position-of-power-picture-id1299077582?b=1&k=20&m=1299077582&s=170667a&w=0&h=Esjqlg_WCWmTc83Dv6PLhwPFwYN9uXoclBn0cUhtS5I=" alt="">
+       <div class="details">
+         <h3>Author</h3>
+         <h4>Nicki Minaj</h4>
+         <h4>musician</h4>
+       </div>
+     </div>
+     <main> 
+       
+      <div class="text">
+      <h2>Windows 10 two-finger scrolling</h2>
+                 <p>
+               Windows 10 has a new two-finger scrolling feature.
+                You can now scroll through your apps with two fingers.
+                This is a great feature for touch-screen devices.
+                You can also use your mouse to scroll through your apps.</p>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus temporibus non cum dolorum. Nesciunt aliquid doloribus optio, repudiandae praesentium minus ea blanditiis in vero alias ipsam deleniti quae voluptates labore harum? Doloremque sapiente repellendus velit. Qui mollitia magnam, atque, debitis quos sequi ducimus ipsa amet ipsum voluptas ut recusandae deserunt beatae nemo reprehenderit, illo quod delectus magni rem repellat? Sed ea, voluptatem nihil nostrum placeat veniam voluptates animi soluta optio harum iste veritatis fugit nisi amet voluptatum eveniet asperiores ipsum aliquid consequuntur magni, nam beatae pariatur. Perferendis voluptates laudantium saepe ex minima laborum voluptatum asperiores illo doloremque, ad veritatis facilis?</p>
+      </div>
+     </main>
   </div>
 </template>
 
--- src/views/windows10.vue
+++ src/views/windows10.vue
@@ -8,8 +8,17 @@
          <h4>musician</h4>
        </div>
      </div>
-     <main>
+     <main> 
+       
+      <div class="text">
+      <h2>Windows 10 two-finger scrolling</h2>
+                 <p>
+               Windows 10 has a new two-finger scrolling feature.
+                You can now scroll through your apps with two fingers.
+                This is a great feature for touch-screen devices.
+                You can also use your mouse to scroll through your apps.</p>
        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus temporibus non cum dolorum. Nesciunt aliquid doloribus optio, repudiandae praesentium minus ea blanditiis in vero alias ipsam deleniti quae voluptates labore harum? Doloremque sapiente repellendus velit. Qui mollitia magnam, atque, debitis quos sequi ducimus ipsa amet ipsum voluptas ut recusandae deserunt beatae nemo reprehenderit, illo quod delectus magni rem repellat? Sed ea, voluptatem nihil nostrum placeat veniam voluptates animi soluta optio harum iste veritatis fugit nisi amet voluptatum eveniet asperiores ipsum aliquid consequuntur magni, nam beatae pariatur. Perferendis voluptates laudantium saepe ex minima laborum voluptatum asperiores illo doloremque, ad veritatis facilis?</p>
+      </div>
      </main>
   </div>
 </template>
